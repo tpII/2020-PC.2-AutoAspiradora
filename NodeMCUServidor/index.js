8fcc64c2d95dcfd7c21e9b5a24e8ba9c8bd037a2
Juan <juanman.g97@gmail.com>
2020-11-17 19:13:21 -0300

    Se crea archivo fuente y cabecera para enviar datos al servidor Node.js, utilizando la libreria cURL

--- NodeMCUServidor/index.js
+++ NodeMCUServidor/index.js
@@ -4,12 +4,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+
 app.listen(3000, () => console.log("Servidor funcionando en puerto 3000"));
 
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
+app.post('/curl', (req, res) => {
+    console.log(req.body);
+    res.status(200).send("Informacion con curl recibida");
+});
 
 app.get('/',(req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-})
+});
 
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/icons', express.static(__dirname + '/node_modules/bootstrap-icons/icons'));
---
+++ comunicacion_hilos_servidor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <curl/curl.h>
+#include <string.h>
+#include "include/comunicacion_hilos_servidor.h"
+
+CURL * configurar_conexion(char *);
+int enviar_datos(CURL *, datos_recorrido);
+int cerrar_conexion(CURL *);
+
+
+CURL * configurar_conexion(char * url){
+    CURL * curl = curl_easy_init();
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    return curl;
+}
+
+int enviar_datos(CURL * curl, datos_recorrido datos){
+    char campos[80];
+    sprintf(campos,"x=%f&y=%f&vel_max=%f&distancia=%f",
+        datos.obstaculo_x,datos.obstaculo_y,datos.vel_max,datos.distancia);
+    puts(campos);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, campos);
+    curl_easy_perform(curl);
+}
+
+int cerrar_conexion(CURL * curl){
+    curl_easy_cleanup(curl);
+}
